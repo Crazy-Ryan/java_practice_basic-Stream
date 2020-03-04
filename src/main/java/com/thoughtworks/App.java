ca39fe7830ad7d1c76ddc308be051e2814ad9453
Ryan.Wang <ranwang1995@outlook.com>
2020-03-04 18:47:00 +0800

    [WangRan] feat-3: finish the method to get all traders from Cambridge and sort with their names

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -57,7 +57,8 @@ public class App {
     }
 
     public static List<Trader> getCambridgeTraders(List<Transaction> transactions) {
-        return Collections.emptyList();
+        return transactions.stream().map(Transaction::getTrader).distinct()
+                .filter(trader -> trader.getCity().equals("Cambridge")).sorted((trader, t1) -> trader.getName().compareTo(t1.getName())).collect(Collectors.toList());
     }
 
     public static List<String> getTradersName(List<Transaction> transactions) {
